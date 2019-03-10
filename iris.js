01384769da30bb0f11bc9f44b461eb4a1e9c5adc
Bob Brown <robertbernardbrown@gmail.com>
2019-03-10 18:10:18 -0400

    beautifies files and gets code to work properly

--- iris.js
+++ iris.js
@@ -4,31 +4,43 @@ import iris from "./iris.json";
 import irisTesting from "./iris-testing.json";
 
 const trainingData = tf.tensor2d(iris.map(item => [
-    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
+  item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]))
 const outputData = tf.tensor2d(iris.map(item => [
-    item.species === "setosa" ? 1 : 0,
-    item.species === "virginica" ? 1 : 0,
-    item.species === "versicolor" ? 1 : 0,
+  item.species === "setosa" ? 1 : 0,
+  item.species === "virginica" ? 1 : 0,
+  item.species === "versicolor" ? 1 : 0,
 ]))
-const testingData = tf.tensor2d(iris.map(item => [
-    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
+const testingData = tf.tensor2d(irisTesting.map(item => [
+  item.sepal_length, item.sepal_width, item.petal_length, item.petal_width
 ]))
 
 
 const model = tf.sequential();
 
 model.add(tf.layers.dense({
-    inputShape: [4],
-    activation: 'sigmoid',
-    units: 5
-}))
+  inputShape: [4],
+  activation: 'sigmoid',
+  units: 5
+}));
 model.add(tf.layers.dense({
-    inputShape: [5],
-    activation: 'sigmoid',
-    units: 3
-}))
+  inputShape: [5],
+  activation: 'sigmoid',
+  units: 3
+}));
 model.add(tf.layers.dense({
-    activation: 'sigmoid',
-    units: 3
-}))
+  activation: 'sigmoid',
+  units: 3
+}));
+
+model.compile({
+  loss: "meanSquaredError",
+  optimizer: tf.train.adam(.06),
+});
+
+model.fit(trainingData, outputData, {
+    epochs: 100
+  })
+  .then((history) => {
+    model.predict(testingData).print();
+  });
